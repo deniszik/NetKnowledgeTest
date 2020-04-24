fd2d9e5374df7e84d3e0a6996a7c7a73b9c01da1
deniszik <den783245@gmail.com>
2020-04-24 12:40:22 +0300

    Fix of cache control

--- Startup.cs
+++ Startup.cs
@@ -1,10 +1,12 @@
 using System;
 using System.Collections.Generic;
+using System.IO.Compression;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.ResponseCompression;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -22,7 +24,16 @@ namespace NetKnowledgeTest
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
-        {           
+        {
+            services.AddResponseCompression(options => options.EnableForHttps = true);
+            services.Configure<BrotliCompressionProviderOptions>(options =>
+            {
+                options.Level = CompressionLevel.Fastest;
+            });
+            services.Configure<GzipCompressionProviderOptions>(options =>
+            {
+                options.Level = CompressionLevel.Fastest;
+            });
 
             services.AddControllersWithViews();
         }
@@ -40,8 +51,14 @@ namespace NetKnowledgeTest
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+            app.UseResponseCompression();
             app.UseHttpsRedirection();
-            app.UseStaticFiles();
+            app.UseStaticFiles(new StaticFileOptions() { 
+                OnPrepareResponse = ctx =>
+                {
+                    ctx.Context.Response.Headers.Add("Cache-Control", "public,max-age=2592000");
+                }
+            });
 
             app.UseRouting();
 
--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,33 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+$("#loginButton").click(function () {
+    var userName = $("#userNameInput").val();
+    var userPassword = $("#userPasswordInput").val();
+    var loginMessageResult = $("#loginMessageResult");
+
+    $.ajax({
+        type: "GET",
+        url:location.origin +"/Home/UserLogin",
+        data: {
+            userName: userName,
+            userPassword: userPassword
+        },
+        contentType: "application/json",
+        dataType: "json"
+    }).done(function (response) {
+        if (typeof response.ResultCode === 'undefined') { // successfully login
+            loginMessageResult.removeClass("alert-danger").addClass("alert-success");
+            loginMessageResult.text("Entity details: " + JSON.stringify(response));
+        } else { // login failed
+            loginMessageResult.removeClass("alert-success").addClass("alert-danger");
+            loginMessageResult.text("Code: " + response.ResultCode + ",  Details: " + response.ResultMessage);
+        }
+    }).fail(function (response) {
+        loginMessageResult.removeClass("alert-success").addClass("alert-danger");
+        loginMessageResult.text("Server error [" + response.status + "]");
+    }).always(function () {
+        loginMessageResult.removeClass("d-none");
+    });
+});
